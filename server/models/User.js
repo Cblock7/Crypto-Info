27287e4c5aeb07c3efd0ab32e096cda4093ca925
LHWOOD <hutsonwood@gmail.com>
2021-06-29 23:33:04 -0600

    created both coin and user models

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,39 @@
 const { Schema, model } = require("mongoose");
+const coinSchema = require("./Coin");
 
 const userSchema = new Schema({
-  name: {
+  userName: {
     type: String,
     required: true,
     unique: true,
     trim: true,
   },
-  skills: [
-    {
-      type: String,
-      trim: true,
-    },
-  ],
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  savedCoins: [coinSchema],
 });
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const User = model("User", userSchema);
 
