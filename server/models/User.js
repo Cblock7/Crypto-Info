993630f1a6d282d99844244ea9d4bb525b40502a
LHWOOD <hutsonwood@gmail.com>
2021-06-30 00:12:25 -0600

    fixed a naming conflict in seeders

--- server/models/User.js
+++ server/models/User.js
@@ -19,21 +19,21 @@ const userSchema = new Schema({
     required: true,
   },
 
-  savedCoins: [coinSchema],
+  // savedCoins: [coinSchema],
 });
 
-userSchema.pre("save", async function (next) {
-  if (this.isNew || this.isModified("password")) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+// userSchema.pre("save", async function (next) {
+//   if (this.isNew || this.isModified("password")) {
+//     const saltRounds = 10;
+//     this.password = await bcrypt.hash(this.password, saltRounds);
+//   }
 
-  next();
-});
+//   next();
+// });
 
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
+// userSchema.methods.isCorrectPassword = async function (password) {
+//   return bcrypt.compare(password, this.password);
+// };
 
 const User = model("User", userSchema);
 
