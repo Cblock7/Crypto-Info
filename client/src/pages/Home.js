53ec970c44fcd159500a44471c36d31b03ce0ff7
Dane Enright <enrightco@comcast.net>
2021-06-30 11:35:37 -0600

    created Signup.js and auth.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Signup from "../components"
+
+const Home = () => {
+    return (
+        <div className = "container">
+            < />
+
+        </div>
+    )
+}
