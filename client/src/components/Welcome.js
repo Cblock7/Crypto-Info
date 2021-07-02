6567ad92f08933ae08d6420d591b1923321801ea
Altan Guc <altan.guc@colorado.edu>
2021-07-02 12:52:05 -0600

    adding logout

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Welcome.css";
+
+function Welcome() {
+  return (
+    <aside>
+      <h2>Welcome to Crypto-Info!</h2>
+      <p>
+        This is the one-stop location for amatuer crypto investors to view
+        crypto prices, track their favorited cryptos, and find sources for
+        beginning your crypto trading journey.
+      </p>
+    </aside>
+  );
+}
+
+export default Welcome;
